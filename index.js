091d9c2b507a1c903bda8750a1b0bbff580eaa8e
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-12-04 19:55:32 +0100

    Added function for changing box color to red

---
+++ index.js
@@ -0,0 +1,7 @@
+let project1_button = document.getElementById("button_project1");
+project1_button.addEventListener("click", changeColor);
+
+function changeColor() {
+  let colored_box = document.getElementById("box_project1");
+  colored_box.style.backgroundColor = "red";
+}
