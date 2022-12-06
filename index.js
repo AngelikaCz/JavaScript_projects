b14c7eed28e91f0da6ba06904ecae1fec85c3f24
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-12-06 20:02:29 +0100

    Added function for counting letters

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function changeColor() {
   let colored_box = document.getElementById("box_project1");
   colored_box.style.backgroundColor = "blue";
 }
+
+let project2_counter = document.getElementById("box_project2");
+project2_counter.addEventListener("change", changeCounter);
+
+function changeCounter(event) {
+  let wordCount = document.getElementById("counter");
+  let typedLetters = document.getElementById("box_project2").value;
+  wordCount.innerHTML = event.target.value.length;
+}
